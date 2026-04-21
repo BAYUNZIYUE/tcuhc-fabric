4663b2943ce8cb3b6f6aad3fda98f3c25d89c8d4
BAYUNZIYUE <BAYUNZIYUE@users.noreply.github.com>
2026-04-21 14:50:31 +0000

    Restore 1.21.1 MARINE generator mixin compatibility
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/main/java/me/fallenbreath/tcuhc/mixins/core/MinecraftServerMixin.java
+++ src/main/java/me/fallenbreath/tcuhc/mixins/core/MinecraftServerMixin.java
@@ -3,27 +3,25 @@ package me.fallenbreath.tcuhc.mixins.core;
 import com.mojang.datafixers.util.Pair;
 import me.fallenbreath.tcuhc.UhcGameManager;
 import me.fallenbreath.tcuhc.UhcGameTeam;
-import me.fallenbreath.tcuhc.task.MsptRecorder;
 import me.fallenbreath.tcuhc.task.TaskPregenerate;
-import net.minecraft.class_1937;
-import net.minecraft.class_1959;
-import net.minecraft.class_2338;
-import net.minecraft.class_2794;
-import net.minecraft.class_2960;
-import net.minecraft.class_3215;
-import net.minecraft.class_3218;
-import net.minecraft.class_3754;
-import net.minecraft.class_4766;
-import net.minecraft.class_5321;
-import net.minecraft.class_5363;
-import net.minecraft.class_5284;
-import net.minecraft.class_5455;
-import net.minecraft.class_6544;
-import net.minecraft.class_6910;
-import net.minecraft.class_6880;
-import net.minecraft.class_6953;
-import net.minecraft.class_7924;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
+import net.minecraft.world.biome.source.util.MultiNoiseUtil;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
+import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
+import net.minecraft.world.gen.densityfunction.DensityFunction;
+import net.minecraft.world.gen.noise.NoiseRouter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -53,7 +51,7 @@ public abstract class MinecraftServerMixin
 			method = "createWorlds",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/server/world/ServerWorld;<init>(Lnet/minecraft/server/MinecraftServer;Ljava/util/concurrent/Executor;Lnet/minecraft/world/level/storage/LevelStorage$Session;Lnet/minecraft/world/level/ServerWorldProperties;Lnet/minecraft/class_5321;Lnet/minecraft/class_5363;Lnet/minecraft/server/WorldGenerationProgressListener;ZJLjava/util/List;ZLnet/minecraft/util/math/random/RandomSequencesState;)V"
+					target = "Lnet/minecraft/server/world/ServerWorld;<init>(Lnet/minecraft/server/MinecraftServer;Ljava/util/concurrent/Executor;Lnet/minecraft/world/level/storage/LevelStorage$Session;Lnet/minecraft/world/level/ServerWorldProperties;Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/world/dimension/DimensionOptions;Lnet/minecraft/server/WorldGenerationProgressListener;ZJLjava/util/List;ZLnet/minecraft/util/math/random/RandomSequencesState;)V"
 			)
 	)
 	private void forceMarineOverworldBiomes(Args args)
@@ -63,78 +61,78 @@ public abstract class MinecraftServerMixin
 			return;
 		}
 
-		class_5321<class_1937> worldKey = args.get(4);
-		if (worldKey != class_1937.field_25179)
+		RegistryKey<World> worldKey = args.get(4);
+		if (worldKey != World.OVERWORLD)
 		{
 			return;
 		}
 
-		class_5363 dimensionOptions = args.get(5);
-		class_2794 chunkGenerator = dimensionOptions.comp_1013();
-		if (!(chunkGenerator instanceof class_3754))
+		DimensionOptions dimensionOptions = args.get(5);
+		ChunkGenerator chunkGenerator = dimensionOptions.chunkGenerator();
+		if (!(chunkGenerator instanceof NoiseChunkGenerator))
 		{
 			return;
 		}
 
-		class_3754 noiseChunkGenerator = (class_3754)chunkGenerator;
-		args.set(5, new class_5363(
-				dimensionOptions.comp_1012(),
+		NoiseChunkGenerator noiseChunkGenerator = (NoiseChunkGenerator)chunkGenerator;
+		args.set(5, new DimensionOptions(
+				dimensionOptions.dimensionTypeEntry(),
 				createMarineChunkGenerator((MinecraftServer)(Object)this, noiseChunkGenerator)
 		));
 		if (!marineGeneratorSwapLogged)
 		{
 			marineGeneratorSwapLogged = true;
-			UhcGameManager.LOG.info("MARINE overworld generator swapped to {} with ocean-family multi-noise biomes", class_3754.class.getSimpleName());
+			UhcGameManager.LOG.info("MARINE overworld generator swapped to {} with ocean-family multi-noise biomes", NoiseChunkGenerator.class.getSimpleName());
 		}
 	}
 
-	private static class_3754 createMarineChunkGenerator(MinecraftServer server, class_3754 noiseChunkGenerator)
+	private static NoiseChunkGenerator createMarineChunkGenerator(MinecraftServer server, NoiseChunkGenerator noiseChunkGenerator)
 	{
-		class_6880<class_5284> originalSettingsEntry = noiseChunkGenerator.method_41541();
-		class_5284 originalSettings = originalSettingsEntry.comp_349();
-		class_6953 originalRouter = originalSettings.comp_477();
+		RegistryEntry<ChunkGeneratorSettings> originalSettingsEntry = noiseChunkGenerator.getSettings();
+		ChunkGeneratorSettings originalSettings = originalSettingsEntry.value();
+		NoiseRouter originalRouter = originalSettings.noiseRouter();
 
 		ConstantDensityFunction deepOcean = new ConstantDensityFunction(-1.0);
 		SubmergedDensityFunction oceanFloor = new SubmergedDensityFunction();
 
-		class_6953 marineRouter = new class_6953(
-				originalRouter.comp_414(),
-				originalRouter.comp_415(),
-				originalRouter.comp_416(),
-				originalRouter.comp_417(),
-				originalRouter.comp_420(),
-				originalRouter.comp_539(),
+		NoiseRouter marineRouter = new NoiseRouter(
+				originalRouter.barrierNoise(),
+				originalRouter.fluidLevelFloodednessNoise(),
+				originalRouter.fluidLevelSpreadNoise(),
+				originalRouter.lavaNoise(),
+				originalRouter.temperature(),
+				originalRouter.vegetation(),
 				deepOcean,
-				originalRouter.comp_423(),
-				originalRouter.comp_424(),
-				originalRouter.comp_485(),
+				originalRouter.erosion(),
+				originalRouter.depth(),
+				originalRouter.ridges(),
 				oceanFloor,
 				oceanFloor,
-				originalRouter.comp_428(),
-				originalRouter.comp_429(),
-				originalRouter.comp_430()
+				originalRouter.veinToggle(),
+				originalRouter.veinRidged(),
+				originalRouter.veinGap()
 		);
 
-		class_5284 marineSettings = new class_5284(
-				originalSettings.comp_474(),
-				originalSettings.comp_475(),
-				originalSettings.comp_476(),
+		ChunkGeneratorSettings marineSettings = new ChunkGeneratorSettings(
+				originalSettings.generationShapeConfig(),
+				originalSettings.defaultBlock(),
+				originalSettings.defaultFluid(),
 				marineRouter,
-				originalSettings.comp_478(),
-				originalSettings.comp_538(),
-				originalSettings.comp_479(),
-				originalSettings.comp_480(),
-				originalSettings.comp_481(),
-				originalSettings.comp_482(),
-				originalSettings.comp_483()
+				originalSettings.surfaceRule(),
+				originalSettings.spawnTarget(),
+				originalSettings.seaLevel(),
+				originalSettings.mobGenerationDisabled(),
+				originalSettings.aquifers(),
+				originalSettings.oreVeins(),
+				originalSettings.usesLegacyRandom()
 		);
 
-		class_6880<class_5284> marineSettingsEntry = class_6880.method_40223(marineSettings);
+		RegistryEntry<ChunkGeneratorSettings> marineSettingsEntry = RegistryEntry.of(marineSettings);
 
-		return new class_3754(createMarineBiomeSource(server), marineSettingsEntry);
+		return new NoiseChunkGenerator(createMarineBiomeSource(server), marineSettingsEntry);
 	}
 
-	private static class ConstantDensityFunction implements class_6910.class_6913
+	private static class ConstantDensityFunction implements DensityFunction.Base
 	{
 		private final double value;
 
@@ -144,53 +142,41 @@ public abstract class MinecraftServerMixin
 		}
 
 		@Override
-		public double method_40464(class_6910.class_6912 pos)
+		public double sample(DensityFunction.NoisePos pos)
 		{
 			return value;
 		}
 
 		@Override
-		public void method_40470(double[] densities, class_6910.class_6911 filler)
-		{
-			java.util.Arrays.fill(densities, value);
-		}
-
-		@Override
-		public class_6910 method_40469(class_6910.class_6915 visitor)
-		{
-			return this;
-		}
-
-		@Override
-		public double comp_377()
+		public double minValue()
 		{
 			return value;
 		}
 
 		@Override
-		public double comp_378()
+		public double maxValue()
 		{
 			return value;
 		}
 
 		@Override
-		public net.minecraft.class_7243<? extends class_6910> method_41062()
+		public net.minecraft.util.dynamic.CodecHolder<? extends DensityFunction> getCodecHolder()
 		{
 			throw new UnsupportedOperationException();
 		}
 	}
 
-	private static class SubmergedDensityFunction implements class_6910.class_6913
+	private static class SubmergedDensityFunction implements DensityFunction.Base
 	{
 		private static final double CENTER_Y = 48.0;
 		private static final double AMPLITUDE = 23.0;
 
 		@Override
-		public double method_40464(class_6910.class_6912 pos)
+		public double sample(DensityFunction.NoisePos pos)
 		{
-			int x = pos.comp_371();
-			int y = pos.comp_372();
-			int z = pos.comp_373();
+			int x = pos.blockX();
+			int y = pos.blockY();
+			int z = pos.blockZ();
 
 			double floorY = CENTER_Y + terrainHeight(x, z);
 			double diff = floorY - y;
@@ -234,42 +220,27 @@ public abstract class MinecraftServerMixin
 		}
 
 		@Override
-		public void method_40470(double[] densities, class_6910.class_6911 filler)
-		{
-			for (int i = 0; i < densities.length; i++)
-			{
-				densities[i] = method_40464(filler.method_40477(i));
-			}
-		}
-
-		@Override
-		public class_6910 method_40469(class_6910.class_6915 visitor)
-		{
-			return this;
-		}
-
-		@Override
-		public double comp_377()
+		public double minValue()
 		{
 			return -0.5;
 		}
 
 		@Override
-		public double comp_378()
+		public double maxValue()
 		{
 			return 0.5;
 		}
 
 		@Override
-		public net.minecraft.class_7243<? extends class_6910> method_41062()
+		public net.minecraft.util.dynamic.CodecHolder<? extends DensityFunction> getCodecHolder()
 		{
 			throw new UnsupportedOperationException();
 		}
 	}
 
-	private static class_4766 createMarineBiomeSource(MinecraftServer server)
+	private static MultiNoiseBiomeSource createMarineBiomeSource(MinecraftServer server)
 	{
-		List<Pair<class_6544.class_4762, class_6880<class_1959>>> entries = new ArrayList<>();
+		List<Pair<MultiNoiseUtil.NoiseHypercube, RegistryEntry<Biome>>> entries = new ArrayList<>();
 
 		addMarineBiome(entries, server, -1.0F, -0.6F, "deep_frozen_ocean", true);
 		addMarineBiome(entries, server, -1.0F, -0.6F, "frozen_ocean", false);
@@ -283,47 +254,46 @@ public abstract class MinecraftServerMixin
 		addMarineBiome(entries, server, 0.25F, 0.55F, "warm_ocean", false);
 		addMarineBiome(entries, server, 0.55F, 1.0F, "warm_ocean", false);
 
-		return class_4766.method_49501(new class_6544.class_6547(entries));
+		return MultiNoiseBiomeSource.create(new MultiNoiseUtil.Entries<>(entries));
 	}
 
-	private static void addShoreBiome(List<Pair<class_6544.class_4762, class_6880<class_1959>>> entries, MinecraftServer server, float minTemperature, float maxTemperature, String biomeId)
+	private static void addShoreBiome(List<Pair<MultiNoiseUtil.NoiseHypercube, RegistryEntry<Biome>>> entries, MinecraftServer server, float minTemperature, float maxTemperature, String biomeId)
 	{
 		entries.add(Pair.of(
-				class_6544.method_38118(
-						class_6544.class_6546.method_38121(minTemperature, maxTemperature),
-						class_6544.class_6546.method_38121(-1.0F, 1.0F),
-						class_6544.class_6546.method_38121(-0.15F, 0.35F),
-						class_6544.class_6546.method_38121(-1.0F, 1.0F),
-						class_6544.class_6546.method_38121(0.2F, 1.0F),
-						class_6544.class_6546.method_38121(-1.0F, 1.0F),
+				MultiNoiseUtil.createNoiseHypercube(
+						MultiNoiseUtil.ParameterRange.of(minTemperature, maxTemperature),
+						MultiNoiseUtil.ParameterRange.of(-1.0F, 1.0F),
+						MultiNoiseUtil.ParameterRange.of(-0.15F, 0.35F),
+						MultiNoiseUtil.ParameterRange.of(-1.0F, 1.0F),
+						MultiNoiseUtil.ParameterRange.of(0.2F, 1.0F),
+						MultiNoiseUtil.ParameterRange.of(-1.0F, 1.0F),
 						0.0F
 				),
 				getBiome(server, biomeId)
 		));
 	}
 
-	private static void addMarineBiome(List<Pair<class_6544.class_4762, class_6880<class_1959>>> entries, MinecraftServer server, float minTemperature, float maxTemperature, String biomeId, boolean deep)
+	private static void addMarineBiome(List<Pair<MultiNoiseUtil.NoiseHypercube, RegistryEntry<Biome>>> entries, MinecraftServer server, float minTemperature, float maxTemperature, String biomeId, boolean deep)
 	{
 		entries.add(Pair.of(
-				class_6544.method_38118(
-						class_6544.class_6546.method_38121(minTemperature, maxTemperature),
-						class_6544.class_6546.method_38121(-1.0F, 1.0F),
-						class_6544.class_6546.method_38121(-1.2F, deep ? -0.45F : 0.05F),
-						class_6544.class_6546.method_38121(-1.0F, 1.0F),
-						class_6544.class_6546.method_38121(deep ? -1.0F : 0.0F, deep ? -0.2F : 1.0F),
-						class_6544.class_6546.method_38121(-1.0F, 1.0F),
+				MultiNoiseUtil.createNoiseHypercube(
+						MultiNoiseUtil.ParameterRange.of(minTemperature, maxTemperature),
+						MultiNoiseUtil.ParameterRange.of(-1.0F, 1.0F),
+						MultiNoiseUtil.ParameterRange.of(-1.2F, deep ? -0.45F : 0.05F),
+						MultiNoiseUtil.ParameterRange.of(-1.0F, 1.0F),
+						MultiNoiseUtil.ParameterRange.of(deep ? -1.0F : 0.0F, deep ? -0.2F : 1.0F),
+						MultiNoiseUtil.ParameterRange.of(-1.0F, 1.0F),
 						0.0F
 				),
 				getBiome(server, biomeId)
 		));
 	}
 
-	private static class_6880<class_1959> getBiome(MinecraftServer server, String biomeId)
+	private static RegistryEntry<Biome> getBiome(MinecraftServer server, String biomeId)
 	{
-		Optional<?> biome = server.method_30611()
-				.method_30530(class_7924.field_41236)
-				.method_40264(class_5321.method_29179(class_7924.field_41236, class_2960.method_60655("minecraft", biomeId)));
-		return (class_6880<class_1959>)biome.orElseThrow(IllegalStateException::new);
+		Registry<Biome> biomeRegistry = server.getRegistryManager().get(RegistryKeys.BIOME);
+		RegistryKey<Biome> biomeKey = RegistryKey.of(RegistryKeys.BIOME, Identifier.of("minecraft", biomeId));
+		return biomeRegistry.getEntry(biomeKey).orElseThrow(IllegalStateException::new);
 	}
 
 	@Inject(
@@ -337,10 +307,10 @@ public abstract class MinecraftServerMixin
 	)
 	private void setSpawnPosTo00(CallbackInfo ci)
 	{
-		class_3218 world = this.uhcGameManager.getOverWorld();
-		class_2338 spawnPos = class_2338.field_10980.method_10086(world.method_14178().method_12129().method_12100(world));
-		float spawnAngle = world.method_43127();
-		world.method_8554(spawnPos, spawnAngle);
+		ServerWorld world = this.uhcGameManager.getOverWorld();
+		BlockPos spawnPos = BlockPos.ORIGIN.up(world.getChunkManager().getChunkGenerator().getSpawnHeight(world));
+		float spawnAngle = world.getSpawnAngle();
+		world.setSpawnPos(spawnPos, spawnAngle);
 	}
 
 	@Inject(method = "tick", at = @At(value = "HEAD"))
@@ -413,17 +383,17 @@ public abstract class MinecraftServerMixin
 			return;
 		}
 
-		java.util.List<class_2338> spawnPositions = new java.util.ArrayList<>();
+		java.util.List<BlockPos> spawnPositions = new java.util.ArrayList<>();
 		for (int i = 0; i < teamCount; i++)
 		{
 			double angle = i * (360.0 / teamCount) * Math.PI / 180;
 			int x = (int)(border * Math.sin(angle));
 			int z = (int)(border * Math.cos(angle));
-			spawnPositions.add(new class_2338(x, 64, z));
+			spawnPositions.add(new BlockPos(x, 64, z));
 		}
 
 		int radius = borderStart / 32 + 5;
-		class_3218 overworld = this.uhcGameManager.getOverWorld();
+		ServerWorld overworld = this.uhcGameManager.getOverWorld();
 		TaskPregenerate.reprioritizeOverworld((MinecraftServer)(Object)this, radius, overworld, spawnPositions);
 	}
 }
